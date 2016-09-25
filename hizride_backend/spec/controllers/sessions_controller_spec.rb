9a0ba63633c8026fc48a28eca75c70af2712474f
Topi T <smobey@gmail.com>
2016-09-25 16:01:38 +0300

    testauksen testailua

--- hizride_backend/spec/controllers/sessions_controller_spec.rb
+++ hizride_backend/spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  before do
+    @user = User.create
+  end
+
+  it 'should return the role set' do
+    @user.set_role('driver')
+    expect(@user.get_role).to eq('driver')
+  end
+
 end
