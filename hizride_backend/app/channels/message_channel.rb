95efa44c73366e99217732268edb2e3238fadf4c
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2016-11-02 01:15:43 +0200

    Actioncables websocket initial commit

--- hizride_backend/app/channels/message_channel.rb
+++ hizride_backend/app/channels/message_channel.rb
@@ -1,10 +1,17 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class MessageChannel < ApplicationCable::Channel
   def subscribed
+    @params = params
     # stream_from "some_channel"
+    logger.info ">>> Subscribed #{@params}!"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    @params = params
+    logger.info ">>> Unsubscribed #{@params}!"
+  end
+
+  def message(data)
+    logger.info "DATA>> #{data}"
   end
 end
