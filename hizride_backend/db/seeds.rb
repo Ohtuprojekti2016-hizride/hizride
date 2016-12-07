0dfed648ba4607fd4f3b6023410e9bc3c8fc6f53
Vilma <vilma.l.kangas@helsinki.fi>
2016-12-07 12:36:06 +0200

    rake db:seed

--- hizride_backend/db/seeds.rb
+++ hizride_backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#pasilan asema
+User.find(4).update(:destination_lat => "60.1987823", :destination_lng => 24.933358399999975)
+#itäkeskus
+User.find(7).update(:destination_lat => "60.2100092", :destination_lng => 25.081188200000042)
+#Kamppi
+User.find(12).update(:destination_lat => "60.16745770000001", :destination_lng => 24.931074800000033)
+#Otaniemi
+User.find(13).update(:destination_lat => "60.1841396", :destination_lng => 24.83008380000001)
